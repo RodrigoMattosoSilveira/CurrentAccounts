e775d4f797324007f90f5e2446c54ca0a0d8e828
RodrigoMattosoSilveira <r3850365@gmail.com>
2025-11-14 09:56:31 -0800

    Setting the static path correctly

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"path/filepath"
 
+	"github.com/RodrigoMattosoSilveira/mygo/internal/utilities"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,7 +13,7 @@ func SetupRouter()  *gin.Engine {
   router :=gin.Default()
 
     // Determine project root (2 levels up from cmd/web)
-    projectRoot, err := filepath.Abs(filepath.Join(filepath.Dir("."), "../../"))
+    projectRoot, err := utilities.FindProjectRoot()
     if err != nil {
         log.Fatalf("failed to resolve project root: %v", err)
     }
