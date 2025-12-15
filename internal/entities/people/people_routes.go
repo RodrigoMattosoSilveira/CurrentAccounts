2a1919989b7ee3b9327ddad2e6cbfad2518ce0e6
Rodrigo Silveira <r3850365@gmail.com>
2025-12-15 13:47:43 -0800

    will only have an update capability for now

--- internal/entities/people/people_routes.go
+++ internal/entities/people/people_routes.go
@@ -10,11 +10,10 @@ func RegisterRoutes(r *fiber.App, db *gorm.DB) {
 	service := NewService(repo)
 	controller := NewController(service)
 
-	r.Get("/people", controller.Index)
-	r.Get("/people/new", controller.New)
-	r.Post("/people", controller.Create)
+		r.Get("/people", controller.Index)
+		r.Get("/people/new", controller.New)
+		r.Post("/people", controller.Create)
 	r.Get("/people/:id", controller.Show)
-	r.Get("/people/:id/edit", controller.Edit)
 	r.Post("/people/:id", controller.Update)
 	r.Post("/people/:id/delete", controller.Delete)
 }
